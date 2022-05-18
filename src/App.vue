0f8c0d45fb975815a9a83853b9992ab536fa8cb7
NinFrei <n.freiman@yahoo.com>
2022-05-18 16:54:16 +0200

    refresh Data und mouted gemacht

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="app">
     <h1 id="title">{{ title }}</h1>
+    <h1 class="list-group-item" >
+      <strong></strong>{{ currentDate()}}
+    </h1>
 
-    <ul>
-        <h1 class="list-group-item" v-schow="currentDate">
-        <strong></strong>{{ currentDate }}
-      </h1>
-      <li class="entry-list" v-for="entry in entries.slice(1)" :key="entry">
-        <span class="entry-time">{{ entry[0] }}, {{ entry[1] }}</span>
-        <h2 class="entry-time">{{ entry[2] }}</h2>
-        <h3 class="entry-time">{{ entry[3] }}</h3>
+    <ul v-if="entries">
+      <li class="entry-list" v-for="entry in entries" :key="entry.id">
+        <span class="entry-time"
+          >{{ entry[0] }}, {{ entry[1].replaceAll("/", ".") }}</span
+        >
+        <h2 class="entry-titele">{{ entry[2] }}</h2>
+        <h3 class="entry-text">{{ entry[3] }}</h3>
         <br />
       </li>
     </ul>
+    <h1 v-else>NO EVENTS; SORRY!</h1>
 
     <div id="footer">
-        <img :src="imgLink1" />
+      <img :src="imgLink1" />
 
-        <img v-bind:src="imgLink2" />
+      <img v-bind:src="imgLink2" />
 
-        <img :src="imgLink3" />
+      <img :src="imgLink3" />
     </div>
   </div>
 
@@ -76,42 +79,58 @@ export default {
   name: "App",
   data() {
     return {
-        title: "Welcome to Opportunity",
-        currentDate: "",
-        sheet_id: "1a81aI0Y8ViZO0tI92h2YSMqVQJ8hmNNMyMylXgvwiU4",
-        api_token: "AIzaSyA-qeDXOhEeQDA0vQf7LgkF7DQtGnAtmAU",
-        entries: [],
-        imgLink1: require("../src/assets/stadt_zürich.png"),
-        // logoWidth1: 100,
-        imgLink2: require("../src/assets/opportunity.png"),
-        // logoWidth2: 100,
-        imgLink3: require("../src/assets/sag.png"),
-        // logoWidth3: 100, */
-      };
-    },
+      title: "Welcome to Opportunity",
+      sheet_id: "1a81aI0Y8ViZO0tI92h2YSMqVQJ8hmNNMyMylXgvwiU4",
+      api_token: "AIzaSyA-qeDXOhEeQDA0vQf7LgkF7DQtGnAtmAU",
+      entries: [],
+      imgLink1: require("../src/assets/stadt_zürich.png"),
+      // logoWidth1: 100,
+      imgLink2: require("../src/assets/opportunity.png"),
+      // logoWidth2: 100,
+      imgLink3: require("../src/assets/sag.png"),
+      // logoWidth3: 100, */
+    };
+  },
   computed: {
-      gsheet_url() {
-        return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A1%3AE100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
-      },
+    gsheet_url() {
+      return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A2%3AE100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
     },
+  },
   methods: {
-      getData() {
-        axios.get(this.gsheet_url).then((response) => {
+    getData() {
+      axios.get(this.gsheet_url).then((response) => {
         this.entries = response.data.valueRanges[0].values;
-        });
-      },
-    /*updateCurrentDate() {},
-  },*/
-      printcurrentDate: function () {
-        return new Date().toLocaleDateString();
-      },
+      });
     },
 
-     mounted() {
-        this.getData();
-        this.currentDate = this.printcurrentDate();
+    currentDate() {
+      const current = new Date();
+      const day = current.getDate();
+      const month = current.getMonth() + 1;
+      const year = current.getFullYear();
+      const dateTime = day + "." + month + "." + year;
+
+      if (month < 10) {
+        return day + "." + "0" + month + "." + year;
+      }
+      return dateTime;
     },
-  };
+
+    refreshData() {
+      this.currentDate();
+      this.getData();
+    },
+  },
+
+  mounted() {
+
+    this.refreshData();
+    setInterval(() => {
+      this.refreshData();
+    }, 1800000);
+    //this.currentDate = this.printcurrentDate();
+  },
+};
 </script>
 
 <style>
@@ -120,6 +139,7 @@ export default {
 #app {
   font-family: "Inter", Arial, Helvetica, sans-serif;
   margin: 60px;
+  list-style-type: none;
 }
 
 body {
@@ -138,14 +158,21 @@ body {
 }*/
 
 .list-group-item {
-  color: rgb(135, 124, 127);
+  color: rgb(136, 124, 127);
   text-align: left;
+  font-size: 50px;
+  padding: 40px;
 }
 
 h1 {
-  color: rgb(0, 0, 0);
+  color: rgb(14, 13, 13);
   text-align: auto;
   margin: 50px;
+}
+
+.entry-time {
+  color: red;
+  font-size: 40px;
 }
 
 .entry-list {
@@ -154,6 +181,7 @@ h1 {
   color: rgb(244, 136, 183);
   font: medium;
   background-color: rgb(109, 55, 196);
+
   width: auto;
   border: 0px;
   padding: 20px;
